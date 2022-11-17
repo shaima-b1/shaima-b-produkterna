ae126473a26cd9ce885152c389a1682d020ff007
shaimabodaka <shaima.bodaka.gbg22@amerikanskagymnasiet.se>
2022-11-17 12:19:11 +0100

    sbjewellery företag
    shaima-b-produkterna

---
+++ slideshow.js
@@ -0,0 +1,21 @@
+var imgArray = [
+    './slideshowpic1.jpg',
+    './slideshowpic2.jpg',
+    './slideshowpic3.jpg',
+    './slideshowpic4.jpg',
+    './slideshowpic5.jpg',
+    './slideshowpic6.jpg',
+    './slideshowpic7.jpg',
+    './slideshowpic8.jpg',
+
+];
+var curIndex = 0;
+var imgDuration = 1000;
+
+function slideShow() {
+    document.getElementById('image1').src = imgArray[curIndex];
+    curIndex++;
+    if (curIndex == imgArray.length) { curIndex = 0; }
+    setTimeout("slideShow()", imgDuration);
+}
+slideShow();
